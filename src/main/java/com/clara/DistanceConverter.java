9f2f60b15bdcb2e82f18d581f9367a000b92670a
alyssadmoore <alyssadmoore@gmail.com>
2016-09-06 12:01:03 -0500

    Completed meters to feet method

--- src/main/java/com/clara/DistanceConverter.java
+++ src/main/java/com/clara/DistanceConverter.java
@@ -5,28 +5,23 @@ import java.util.Scanner;
 public class DistanceConverter {
 
     static Scanner numberScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // write your code here
 
         System.out.println("Please type in a distance in meters:");
         int meters = numberScanner.nextInt();
 
         int feet = metersToFeet(meters);
 
-        System.out.println("In feet, that's " + feet + " ft");
+        System.out.println("That's " + feet + " ft");
 
-        // End of your code here
         numberScanner.close();
     }
 
-
     public static int metersToFeet(int meters){
-        //TODO! This method doesn't work yet. Can you fix it?
         //To convert meters to feet, multiply by 3
-
-        return 0;
+        return (meters * 3);
     }
-
-    // I know doubles would be more real world but we are keeping it simple
-    // int values are a little easier to test - doubles/float are not as precise
+    /* I know doubles would be more real world but we are keeping it simple
+     int values are a little easier to test - doubles/float are not as precise */
 }
